38d01fc4da51aa88d92d145c69e40521ded6b2ad
mandarine-dev <f1tch@proton.me>
2023-07-30 15:21:37 +0200

    add every categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
-  { name: 'How it works', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Roadmap', href: '#' },
-  { name: 'Tokenomics', href: '#' },
-  { name: 'FAQs', href: '#' },
+  { name: 'How it works', href: '#how' },
+  { name: 'Features', href: '#features' },
+  { name: 'Roadmap', href: '#roadmap' },
+  { name: 'Tokenomics', href: '#tokenomics' },
+  { name: 'FAQs', href: '#faq' },
 ];
 
 // https://i.imgur.com/nR7P8kz.png
@@ -98,14 +98,6 @@ export default function Home() {
                     </a>
                   ))}
                 </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
               </div>
             </div>
           </Dialog.Panel>
@@ -116,7 +108,7 @@ export default function Home() {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <div className="text-center">
             <img src="https://i.imgur.com/HsuYpCQ.png" alt="" />
-            <p className="mt-6 text-4xl leading-8 text-zinc-100 cairo">
+            <p className="mt-12 pb-6 text-4xl leading-8 text-zinc-100 cairo">
               HELLO, RABBITS
               <br />
               READY FOR MAGIC?
@@ -135,7 +127,10 @@ export default function Home() {
               </a> */}
             </div>
 
-            <div className="h-96 mt-32 w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200">
+            <div
+              id="how"
+              className="h-96 mt-32 w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200"
+            >
               <p className="pt-14 text-4xl leading-8 text-zinc-100 cairo">
                 HOW IT WORKS
               </p>
@@ -172,7 +167,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="flex flex-col items-center">
+            <div id="features" className="flex flex-col items-center">
               <p className="pt-24 text-lg leading-8 text-zinc-100">
                 The solution to your problems
               </p>
@@ -204,7 +199,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div>
+            <div id="roadmap">
               <p className="pt-24 pb-12 text-4xl leading-8 text-zinc-100 cairo">
                 ROADMAP
               </p>
@@ -215,10 +210,12 @@ export default function Home() {
                 </div>
                 <div className="text-start h-46 w-96 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200">
                   <ul className="text-white p-4">
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">🕒 this is my text</li>
+                    <li className="p-2">🕒 MagiBot MVP release</li>
+                    <li className="p-2">🕒 $MAGI launch with full utilities</li>
+                    <li className="p-2">🕒 Multichain</li>
+                    <li className="p-2">🕒 Prototype Premium Features</li>
+                    <li className="p-2">🕒 Launch referral program</li>
+                    <li className="p-2">🕒 Launch ambassador program</li>
                   </ul>
                 </div>
               </div>
@@ -228,10 +225,24 @@ export default function Home() {
                 </div>
                 <div className="text-start h-46 w-96 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200">
                   <ul className="text-white p-4">
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">🕒 this is my text</li>
+                    <li className="p-2">
+                      🕒 Add more advanced encryption privacy
+                    </li>
+                    <li className="p-2">
+                      🕒 Perfect the referral program to encourage user growth
+                    </li>
+                    <li className="p-2">
+                      🕒 Add lending and borrowing for users
+                    </li>
+                    <li className="p-2">
+                      🕒 Develop portfolio management feature for users
+                    </li>
+                    <li className="p-2">
+                      🕒 Integrate more chains & protocols
+                    </li>
+                    <li className="p-2">
+                      🕒 Develop Partnerships to grow user base
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -241,49 +252,193 @@ export default function Home() {
                 </div>
                 <div className="text-start h-46 w-96 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200">
                   <ul className="text-white p-4">
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">✅ this is my text</li>
-                    <li className="p-2">🕒 this is my text</li>
+                    <li className="p-2">
+                      🕒 Implement additional security measures, such as
+                      two-factor authentication and encryption
+                    </li>
+                    <li className="p-2">
+                      🕒 Optimize the bot{"'"}s performance and scalability for
+                      handling a growing user base
+                    </li>
                   </ul>
                 </div>
               </div>
             </div>
 
-            <div className="h-96 mt-32 w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200">
-              <p className="pt-14 text-4xl leading-8 text-zinc-100 cairo">
+            <div
+              id="tokenomics"
+              className="h-auto py-24 mt-32 w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200"
+            >
+              <p className="-mt-14 pb-14 text-4xl leading-8 text-zinc-100 cairo">
                 TOKENOMICS
               </p>
-              <div className="-mt-14 flex items-center h-full justify-evenly">
-                <div className="flex flex-col items-center">
-                  <img
-                    className="h-24 w-14"
-                    src="https://i.imgur.com/16P45r7.png"
-                    alt=""
-                  />
-                  <p className="text-lg text-white">Telegram</p>
-                  <p className="text-sm text-white">Follow instructions</p>
+              <div className="flex items-center h-full justify-evenly text-white flex-wrap">
+                <div className="text-left space-y-3 mx-12 mb-4">
+                  <div className="cairo text-xl">
+                    $MAGI <span className="text-slate-300">TOKEN</span>
+                  </div>
+                  <div className="text-lg font-bold">
+                    Total supply: 10,000,000 $MAGI
+                  </div>
+                  <div>
+                    <b>Buy/Sell tax:</b> 5% tax applied to each transaction of
+                    $MAGI
+                  </div>
+                  <div>
+                    <b>First 6 months:</b> 1% for LP, 4% for team
+                  </div>
+                  <div>
+                    <b>After 6 months:</b> 1% for LP, 2% for revenue sharing, 2%
+                    for team
+                  </div>
                 </div>
-                <div className="flex flex-col items-center">
-                  <img
-                    className="h-24 w-auto"
-                    src="https://i.imgur.com/7qqJywh.png"
-                    alt=""
-                  />
-                  <p className="text-lg text-white">Let the magic</p>
-                  <p className="text-sm text-white">
-                    Grab a cup of tea during process
-                  </p>
+                <div className="text-left">
+                  <div className="uppercase pb-4 text-purple-200">
+                    Token distribution
+                  </div>
+                  <div className="space-y-2 pb-4">
+                    <div className="text-xl font-bold">500,000</div>
+                    <div>Team & Founder (5%)</div>
+                    <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+                      <div
+                        className="bg-teal-400 h-1.5 rounded-full dark:bg-blue-500"
+                        style={{ width: '5%' }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="space-y-2 pb-4">
+                    <div className="text-xl font-bold">500,000</div>
+                    <div>CEX Listing (5%)</div>
+                    <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+                      <div
+                        className="bg-gray-700 h-1.5 rounded-full dark:bg-blue-500"
+                        style={{ width: '5%' }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="space-y-2 pb-4">
+                    <div className="text-xl font-bold">4,000,000</div>
+                    <div>Supply added to liquidity (40%)</div>
+                    <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+                      <div
+                        className="bg-orange-300 h-1.5 rounded-full dark:bg-blue-500"
+                        style={{ width: '40%' }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="space-y-2 pb-4">
+                    <div className="text-xl font-bold">5,000,000</div>
+                    <div>Seed (50%)</div>
+                    <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+                      <div
+                        className="bg-red-400 h-1.5 rounded-full dark:bg-blue-500"
+                        style={{ width: '50%' }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="space-y-2">
+                    <div className="uppercase py-4 text-purple-200">
+                      VESTING
+                    </div>
+                    <div>
+                      <b>Team:</b> 0% TGE, 6 months cliff, linear for 9 months
+                    </div>
+                    <div>
+                      <b>Seed:</b> 33% TGE, linear for 1 months
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col items-center">
-                  <img
-                    className="h-24 w-auto"
-                    src="https://i.imgur.com/nR7P8kz.png"
-                    alt=""
-                  />
-                  <p className="text-lg text-white">Receive</p>
-                  <p className="text-sm text-white">Get your tokens back</p>
+              </div>
+            </div>
+
+            <div id="faq" className="pt-14">
+              <div className="cairo text-white text-4xl pb-14">FAQs</div>
+              <div className="flex text-white text-left space-x-6">
+                <div className="w-96">
+                  <div className="font-bold pb-4">
+                    What is this Telegram bot?
+                  </div>
+                  <div>
+                    Our Telegram bot is a service that allows for the
+                    anonymization of Ethereum (ETH) funds. It provides users
+                    with a newly generated address to which they can send their
+                    ETH. The bot then sends the funds to a receiver{"'"}s
+                    address, ensuring the transaction remains anonymous.
+                  </div>
                 </div>
+                <div className="w-96">
+                  <div className="font-bold  pb-4">
+                    How long does a transaction take?
+                  </div>
+                  <div>
+                    The transaction time depends on the Ethereum network traffic
+                    at the moment. Our bot will do its best to ensure the
+                    transaction is processed as quickly as possible.
+                  </div>
+                </div>
+                <div className="w-96">
+                  <div className="font-bold  pb-4">
+                    How do I use this service?
+                  </div>
+                  <div>
+                    To use this service, start a conversation with our bot on
+                    Telegram. It will provide you with an Ethereum address where
+                    you can send your funds. After that, give the bot the
+                    receiver{"'"}s Ethereum address. The bot will then anonymize
+                    the transaction for you.
+                  </div>
+                </div>
+                <div className="w-96">
+                  <div className="font-bold  pb-4">
+                    What if I encounter a problem or need assistance?
+                  </div>
+                  <div>
+                    If you encounter any problems or need help, please contact
+                    our support team. We are available 24/7 and are always ready
+                    to assist you.
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="h-96 flex flex-col items-center justify-center">
+              <div className="flex space-x-3">
+                <div className="cursor-pointer">
+                  <a
+                    href="https://magibot.gitbook.io/magibot/roadmap"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://www.svgrepo.com/show/330505/gitbook.svg"
+                      alt=""
+                      className="h-8 w-8 invert"
+                    />{' '}
+                  </a>
+                </div>
+                <div className="cursor-pointer">
+                  <a href="https://twitter.com/MagiBOT_" target="_blank">
+                    <img
+                      src="https://www.svgrepo.com/show/506685/twitter.svg"
+                      alt=""
+                      className="h-8 w-8 invert"
+                    />
+                  </a>
+                </div>
+                <div className="cursor-pointer">
+                  <a
+                    href="https://www.google.com/search?q=coming+soon&sxsrf=AB5stBgo8YungS5qxtNm8leDGNqIi2mWiA%3A1690722972498&source=hp&ei=nGLGZPHsG5rssAec376AAQ&iflsig=AD69kcEAAAAAZMZwrKN0Y851lXEhIXSTjv95LYpqbeIA&ved=0ahUKEwjxqr_xwbaAAxUaNuwKHZyvDxAQ4dUDCAk&uact=5&oq=coming+soon&gs_lp=Egdnd3Mtd2l6Igtjb21pbmcgc29vbjIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMgUQLhiABDIIEAAYgAQYywFI1g1QpQNY1wxwAXgAkAECmAG6AaABmAmqAQM3LjS4AQPIAQD4AQGoAgrCAgcQIxjqAhgnwgIHECMYigUYJ8ICBBAjGCfCAggQABiKBRiRAsICBRAAGIAEwgILEC4YgAQYxwEY0QPCAgoQABiABBgUGIcCwgILEC4YgAQYxwEYrwHCAggQLhiABBjLAcICCxAuGIAEGNQCGMsB&sclient=gws-wiz"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://www.svgrepo.com/show/446808/telegram.svg"
+                      alt=""
+                      className="h-8 w-8 invert"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="cairo text-white pt-4">
+                MagiBot 2023 - All Rights Reserved.
               </div>
             </div>
           </div>
